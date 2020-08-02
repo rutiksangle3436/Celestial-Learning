f088f8a2f42764a2de0dcf1cf9faa8f411cef2b3
Vishal <vishalkale151071@gmail.com>
2020-08-02 19:07:01 +0530

    JWT authetication added with api.

--- Celestial_Learning/urls.py
+++ Celestial_Learning/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import TestToken
 
 urlpatterns = [
     path('',include('home.urls')),
     path('register/',include('register.urls')),
     path('login/',include('login.urls')),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/test/', TestToken.as_view()),
 ]
---
+++ Celestial_Learning/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+# Create your views here.
+
+#curl -X POST -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk2MzYxMzg5LCJqdGkiOiJmNjQzMjUyN2MwMGU0YThiYmYwZGE2N2M3NjFkYzA2OCIsInVzZXJfaWQiOjEwfQ.FvYv7drBdsQMJaXwA02TKe2ZQXt0Eq7fa549J9cGW5Y" http://127.0.0.1:8000/api/token/test/
+class TestToken(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+
+    def post(self, request):
+        content = {'JWT': "is Working",}
+        return Response(content)
